ed2ab1b3d4f162c57df394338dffdb1c3cae64f5
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 01:27:52 +0530

    Update wireColorCode.hpp

--- wireColorCode.hpp
+++ wireColorCode.hpp
@@ -6,8 +6,8 @@ namespace TeleComColorCoder
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-    const char* MajorColorNames[];
-    const char* MinorColorNames[];
+    static char* MajorColorNames[];
+    static char* MinorColorNames[];
 
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
