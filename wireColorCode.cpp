776510c726eeefd34c8938e5ee432ea47309a6bf
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 01:47:11 +0530

    Update wireColorCode.cpp

--- wireColorCode.cpp
+++ wireColorCode.cpp
@@ -7,14 +7,14 @@ namespace TeleComColorCoder
     const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-    ColorPair ColorPair::GetColorFromPairNumber(int pairNumber) {
+    ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return ColorPair(majorColor, minorColor);
     }
 
-    int ColorPair::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
 
