35847763f89e0b6c9d8f12e5387b7da4af67835d
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 02:53:07 +0530

    Update wireColorCode_manual.cpp

--- wireColorCode_manual.cpp
+++ wireColorCode_manual.cpp
@@ -2,19 +2,26 @@
 
 namespace TeleComColorCoder 
 {
+  MajorColor getMajorWireColor() const {
+        return majorWireColor;
+    }
+
+  MinorColor getMinorWireColor() const {
+        return minorWireColor;
+    }
 
   std::string formatColorPairsForMajorColor(MajorColor major) {
         std::string formattedPairs;
-        for (int minor = 0; minor < getNumberOfMinorWireColors(); ++minor) {
-            WireColorPair colorPair = getWireColorPairFromNumber(static_cast<int>(major) * getNumberOfMinorWireColors() + minor + 1);
-            formattedPairs += colorPair.toString() + " - " + std::to_string(getNumberFromWireColorPair(colorPair.getMajorWireColor(), colorPair.getMinorWireColor())) + "\n";
+        for (int minor = 0; minor < numberOfMinorColors; ++minor) {
+            WireColorPair colorPair = getColorFromPairNumber(static_cast<int>(major) * numberOfMinorColors + minor + 1);
+            formattedPairs += colorPair.toString() + " - " + std::to_string(getPairNumberFromColor(colorPair.getMajorWireColor(), colorPair.getMinorWireColor())) + "\n";
         }
         return formattedPairs;
     }
 
     std::string formatColorCodingReferenceManual() {
         std::string referenceManual;
-        for (int major = 0; major < getNumberOfMajorWireColors(); ++major) {
+        for (int major = 0; major < numberOfMajorColors; ++major) {
             referenceManual += formatWireColorCodeManual(static_cast<MajorWireColor>(major));
         }
         return referenceManual;
