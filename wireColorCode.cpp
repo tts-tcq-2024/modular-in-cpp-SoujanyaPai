a7ce77d8ce0bd68e09dd2436a926f300091eac97
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 02:34:38 +0530

    Update wireColorCode.cpp

--- wireColorCode.cpp
+++ wireColorCode.cpp
@@ -2,26 +2,26 @@
 
 namespace TeleComColorCoder 
 {
-    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    const char* majorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+        int numberOfMajorColors = sizeof(majorColorNames) / sizeof(majorColorNames[0]);
+    const char* minorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        int numberOfMinorColors = sizeof(minorColorNames) / sizeof(minorColorNames[0]);
 
-    WireColorPair GetColorFromPairNumber(int pairNumber) {
+    WireColorPair getColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return WireColorPair(majorColor, minorColor);
     }
 
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+    int getPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
 
-    std::string WireColorPair::ToString() {
-        std::string colorPairStr = MajorColorNames[majorColor];
+    std::string WireColorPair::toString() {
+        std::string colorPairStr = majorColorNames[majorColor];
         colorPairStr += " ";
-        colorPairStr += MinorColorNames[minorColor];
+        colorPairStr += minorColorNames[minorColor];
         return colorPairStr;
     }
 }
