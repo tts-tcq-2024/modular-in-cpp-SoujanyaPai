ed291f64ed067485f0031bd1d85f5f44029757f5
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-20 19:41:36 +0530

    Update wireColorCode_manual.cpp

--- wireColorCode_manual.cpp
+++ wireColorCode_manual.cpp
@@ -5,8 +5,11 @@ namespace TeleComColorCoder
   std::string formatColorPairsForMajorColor(MajorColor major) {
         std::string formattedPairs;
         for (int minor = 0; minor < numberOfMinorColors; ++minor) {
-            WireColorPair colorPair = getColorFromPairNumber(static_cast<int>(major) * numberOfMinorColors + minor + 1);
-            formattedPairs += colorPair.toString() + " - " + std::to_string(getPairNumberFromColor(colorPair.getMajorColor(), colorPair.getMinorColor())) + "\n";
+            WireColorPair colorPair = getColorFromPairNumber(
+              static_cast<int>(major) * numberOfMinorColors + minor + 1);
+            formattedPairs += 
+              colorPair.toString() + " - " + std::to_string(getPairNumberFromColor(
+                colorPair.getMajorColor(), colorPair.getMinorColor())) + "\n";
         }
         return formattedPairs;
     }
@@ -14,7 +17,8 @@ namespace TeleComColorCoder
     std::string formatColorCodingReferenceManual() {
         std::string referenceManual;
         for (int major = 0; major < numberOfMajorColors; ++major) {
-            referenceManual += formatColorPairsForMajorColor(static_cast<MajorColor>(major));
+            referenceManual += formatColorPairsForMajorColor(
+              static_cast<MajorColor>(major));
         }
         return referenceManual;
     }
