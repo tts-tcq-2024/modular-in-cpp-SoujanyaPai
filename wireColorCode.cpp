97997d1e204d5598e23f640ae5f161fb2b4dc416
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-20 19:37:56 +0530

    Update wireColorCode.cpp

--- wireColorCode.cpp
+++ wireColorCode.cpp
@@ -2,15 +2,22 @@
 
 namespace TeleComColorCoder 
 {
-    const char* majorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        int numberOfMajorColors = sizeof(majorColorNames) / sizeof(majorColorNames[0]);
-    const char* minorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int numberOfMinorColors = sizeof(minorColorNames) / sizeof(minorColorNames[0]);
+    const char* majorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors = 
+        sizeof(majorColorNames) / sizeof(majorColorNames[0]);
+    const char* minorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"};
+    int numberOfMinorColors = 
+        sizeof(minorColorNames) / sizeof(minorColorNames[0]);
 
     WireColorPair getColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = 
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return WireColorPair(majorColor, minorColor);
     }
 
